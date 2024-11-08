67f95795bf054f032d0040f2b26df0e6b7098c2a
majidbelbasi <majidbelbasi57@gmail.com>
2024-11-08 12:50:14 +0330

    add card componnent

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,7 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `http://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } }
+    }?api_key=${API_KEY}&language=en-US&page=1`
   );
 
   const data = await res.json();
@@ -16,7 +15,6 @@ export default async function Home({ searchParams }) {
     throw new Error("Failed to fetch data");
   }
   const results = data.results;
-  console.log(results);
 
   return (
     <div>
